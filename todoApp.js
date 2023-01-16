0ca4e8ec38b2f783eab1d28f12eb35819252af9a
Jimike110 <oladoyemike@gmail.com>
2023-01-16 14:59:43 +0100

    Made some major responsiveness changes by using @media query to the icons. Also adding a typewriter transition effeect to the prompt text, "What do you want to do today?" and made it responsive by using @media. Couldn't get the localStorage to work.

--- todoApp.js
+++ todoApp.js
@@ -7,12 +7,31 @@ function add() {
     var newLI = document.createElement("li");
     var listItem = document.getElementById("newTodo");
     if (!/\S/.test(listItem.value)) {
-        alert("To-do cannot be blank.");
+        alert("To-do item cannot contain only whitespaces.");
         e.preventDefault();
     }
     var LIcontent = document.createTextNode(listItem.value);   
     newLI.appendChild(LIcontent);
     var ul = document.getElementById("list");
-    ul.appendChild(newLI);
+    var listt = ul.appendChild(newLI);
+    listt;
     document.getElementById("newTodo").value = "";
 }
+// let input = document.getElementById("newTodo");
+
+// input.value = localStorage.getItem("newTodo");
+// let cancel;
+// input.addEventListener("keyup", event => {
+//     if (cancel) clearTimeout(cancel)
+//     cancel = setTimeout(() => {
+//         localStorage.setItem("newTodo", event.target.value)
+//     }, 1000);
+    
+// });
+
+// window.onload = function() {
+//     localStorage.setItem("text", listItem.value());
+// }
+// var text = localStorage.getItem("text");
+// console.log(text);
+
