31443e27accdd39c1f9b805eb85cdac4a0851161
oladoyemike@gmail.com <oladoyemike@gmail.com>
2023-02-10 15:59:28 +0100

    Fixed todoList clearing on page reload by saving page state before reload and loading page state after reload to client's browser

--- todoApp.js
+++ todoApp.js
@@ -1,23 +1,64 @@
-function add() {
-    
-    // var todo = document.getElementById("newTodo").value;
-    // var list = document.getElementById("list").cloneNode();
-    // var ul = document.getElementById("list");
-    // ul.appendChild(todo);
-    var newLI = document.createElement("li");
-    var listItem = document.getElementById("newTodo");
-    if (!/\S/.test(listItem.value)) {
-        document.getElementById("newTodo").value = "";
-        alert("To-do item cannot be empty or contain only whitespaces.");
-        e.preventDefault();
-    }
-    var LIcontent = document.createTextNode(listItem.value);   
-    newLI.appendChild(LIcontent);
-    var ul = document.getElementById("list");
-    var listt = ul.appendChild(newLI);
-    listt;
-    document.getElementById("newTodo").value = "";
+//var button = document.createElement("button");
+
+function saveState() {
+  const todoList = document.getElementById("list").innerHTML;
+  localStorage.setItem("todoList", todoList);
 }
+
+// Load the saved state of the to-do list
+function loadState() {
+  const savedTodoList = localStorage.getItem("todoList");
+  if (savedTodoList) {
+    document.getElementById("list").innerHTML = savedTodoList;
+  }
+}
+
+function add() {
+
+  // var todo = document.getElementById("newTodo").value;
+  // var list = document.getElementById("list").cloneNode();
+  // var ul = document.getElementById("list");
+  // ul.appendChild(todo);
+  var newLI = document.createElement("li");
+  var listItem = document.getElementById("newTodo");
+  if (!/\S/.test(listItem.value)) {
+    document.getElementById("newTodo").value = "";
+    alert("To-do item cannot be empty or contain only whitespaces.");
+    e.preventDefault();
+  }
+  var LIcontent = document.createTextNode(listItem.value);
+
+  //button.innerHTML = '&times';
+  //button.classList.add("clear-btn");
+  //button.onclick = btnSave;
+  //newLI.appendChild(button);
+
+  newLI.appendChild(LIcontent);
+  var ul = document.getElementById("list");
+  var listt = ul.appendChild(newLI);
+  listt;
+  document.getElementById("newTodo").value = "";
+
+  //saveState();
+
+  //newLI.addEventListener("click", function(e) {
+  //if (e.target.classList.contains("clear-btn")) {
+  //var parent = e.target.parentElement;
+  //parent.remove();
+  //}
+  //saveState();
+  //});
+
+  function btnSave() {
+    //var par = e.target.parentElement;
+    newLI.remove();
+  }
+}
+
+// Call the loadState function when the page loads
+window.onbeforeunload = saveState;
+window.onload = loadState;
+
 // let input = document.getElementById("newTodo");
 
 // input.value = localStorage.getItem("newTodo");
@@ -27,7 +68,7 @@ function add() {
 //     cancel = setTimeout(() => {
 //         localStorage.setItem("newTodo", event.target.value)
 //     }, 1000);
-    
+
 // });
 
 // window.onload = function() {
@@ -36,3 +77,9 @@ function add() {
 // var text = localStorage.getItem("text");
 // console.log(text);
 
+// Save the current state of the to-do list
+
+
+// Call the saveState function whenever the to-do list changes
+//const addTodoButton = document.getElementById("btn");
+//addTodoButton.addEventListener("click", saveState);
